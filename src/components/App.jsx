79ff97dc87d904a156631587e16556274e72b192
Larisagabriela329 <larisagabriela329@yahoo.com>
2025-01-19 12:24:27 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Profile from './Profile'; // Adjust path if necessary
-import user from '../data/user.json'; // Adjust path if necessary
+import Profile from './Profile';
+import Statistics from './Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -8,11 +10,11 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
-        flexDirection: 'column',
       }}
     >
       <Profile
@@ -22,6 +24,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
